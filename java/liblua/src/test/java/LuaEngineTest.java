579f1d5a61cb27df7dab82f0e3dfcc0f3d0461d6
yappy <superunyo@yahoo.co.jp>
2018-05-24 23:31:18 +0900

    Simple compile and run & error test

--- java/liblua/src/test/java/LuaEngineTest.java
+++ java/liblua/src/test/java/LuaEngineTest.java
@@ -1,15 +1,45 @@
 import io.github.yappy.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import static org.junit.Assert.*;
 
 public class LuaEngineTest {
 
-	@Test public void newAndClose() {
-		try (LuaEngine lua = new LuaEngine()) {
-			// do nothing
-		}
+	private LuaEngine lua;
+
+	@Before
+	public void init() throws Exception {
+		lua = new LuaEngine();
 	}
 
+	@After
+	public void term() throws Exception {
+		lua.close();
+		lua = null;
+	}
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+
+	@Test
+	public void simpleExecute() throws Exception {
+		lua.execString("x = 1", "simpleExecute.lua");
+	}
+
+	@Test
+	public void syntaxError() throws Exception {
+		exception.expect(LuaSyntaxException.class);
+		lua.execString("a b c", "syntaxError.lua");
+	}
+
+	@Test
+	public void runtimeError() throws Exception {
+		exception.expect(LuaRuntimeException.class);
+		lua.execString("x = nil x(0)", "runtimeError.lua");
+	}
 
 }
