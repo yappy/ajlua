c49804f7a6ffd5ad1e97dd0c7e388d7fd0f9c4ab
yappy <superunyo@yahoo.co.jp>
2018-06-08 00:58:45 +0900

    Android test app

--- android/app/src/main/java/io/github/yappy/androidlua/MainActivity.java
+++ android/app/src/main/java/io/github/yappy/androidlua/MainActivity.java
@@ -1,13 +1,57 @@
 package io.github.yappy.androidlua;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.github.yappy.LuaEngine;
+import io.github.yappy.LuaException;
+import io.github.yappy.LuaPrint;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button button = findViewById(R.id.button3);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				runScript();
+			}
+		});
+	}
+
+	private void runScript() {
+		EditText editText = findViewById(R.id.editText3);
+		String src = editText.getText().toString();
+		try (LuaEngine lua = new LuaEngine()) {
+			lua.openStdLibs();
+			lua.setPrintFunction(new LuaPrint() {
+				@Override
+				public void writeString(String str) {
+					Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+				}
+
+				@Override
+				public void writeLine() {
+
+				}
+			});
+			lua.execString(src, "test.lua");
+		}
+		catch (LuaException e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+		}
+		catch(Exception e){
+			Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+		}
+	}
+
 }
